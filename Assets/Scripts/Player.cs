1c7bfe4835257dcf866c4d19edcb47cc58140d61
Gladwinnnn <gladwin1503@hotmail.com>
2020-05-29 21:44:48 +0800

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    // config
+    [SerializeField] float runSpeed = 5f;
+
+    // State
+    bool isAlive = true;
+
+    // cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // -1 to 1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        {
+            myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+        }
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+}
