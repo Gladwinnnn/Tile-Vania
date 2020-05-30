a94da8f542e3610f46b2afaf7b56a824d7b0c7cd
Gladwinnnn <gladwin1503@hotmail.com>
2020-05-30 23:26:33 +0800

    2nd commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 {
     // config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // State
     bool isAlive = true;
@@ -14,17 +16,25 @@ public class Player : MonoBehaviour
     // cached component references
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    CapsuleCollider myBodyCollider;
+    BoxCollider2D myFeet;
+    float gravityScaleAtStart;
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider>();
+        myFeet = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        Jump();
+        Climb();
     }
 
     void Run()
@@ -36,6 +46,35 @@ public class Player : MonoBehaviour
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         {
             myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+        }
+    }
+
+    void Climb()
+    {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
+        { 
+            myAnimator.SetBool("Climbing", false);
+            myRigidBody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+        myRigidBody.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
+    }
+
+    void Jump()
+    {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity+= jumpVelocityToAdd;
         }
     }
 
